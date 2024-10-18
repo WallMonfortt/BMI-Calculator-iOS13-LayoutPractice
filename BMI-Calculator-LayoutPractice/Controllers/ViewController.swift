8db29c4fec9cf15ac573387d0296a01326c755fe
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2024-10-18 17:44:27 -0600

    slider values working

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var heightView: UILabel!
-    @IBOutlet weak var WeightView: UILabel!
+    @IBOutlet weak var weightView: UILabel!
     @IBAction func heightBarController(_ sender: UISlider) {
+        let height = sender.value
+        heightView.text = String(format: "%.2f", height)
+        print("the value is : \(    sender.value)")
     }
     @IBAction func weightBarController(_ sender: UISlider) {
+        let weight = sender.value
+        weightView.text = String(format: "%.2f", weight)
     }
     
     override func viewDidLoad() {
