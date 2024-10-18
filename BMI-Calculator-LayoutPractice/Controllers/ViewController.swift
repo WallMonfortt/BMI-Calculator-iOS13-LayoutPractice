9489f8efcf8ea180eaa22114357bc875222b3ff6
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2024-10-18 17:24:04 -0600

    layout challenge solved

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightView: UILabel!
+    @IBOutlet weak var WeightView: UILabel!
+    @IBAction func heightBarController(_ sender: UISlider) {
+    }
+    @IBAction func weightBarController(_ sender: UISlider) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
